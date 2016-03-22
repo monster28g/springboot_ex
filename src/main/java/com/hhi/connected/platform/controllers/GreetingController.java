59ff87dbf0f6feb450efd5eebc2dae1ce53a31f2
franky <monster28g@gmail.com>
2016-03-22 17:26:56 +0900

    add web socket server

--- src/main/java/com/hhi/connected/platform/controllers/GreetingController.java
+++ src/main/java/com/hhi/connected/platform/controllers/GreetingController.java
@@ -1,8 +1,10 @@
 package com.hhi.connected.platform.controllers;
 
+import com.hhi.connected.platform.models.HelloMessage;
 import com.hhi.connected.platform.services.MyService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -19,8 +21,14 @@ public class GreetingController {
         return myService.sayHello(name);
     }
 
-    @RequestMapping(value = "/", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-    Object home() {
-        return myService.home();
+//    @RequestMapping(value = "/", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+//    Object home() {
+//        return myService.home();
+//    }
+
+    @MessageMapping("/hello")
+    @SendTo("/topic/greetings")
+    public Object greeting(HelloMessage message) throws Exception {
+        return myService.sayHello("WS, " + message.getName() + "!");
     }
 }
---
+++ src/main/java/com/hhi/connected/platform/models/HelloMessage.java
@@ -0,0 +1,11 @@
+package com.hhi.connected.platform.models;
+
+public class HelloMessage {
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+}
