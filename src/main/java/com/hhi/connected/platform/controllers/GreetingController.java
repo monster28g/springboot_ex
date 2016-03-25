a2d655d10b71837431cc7706b8225af71b8a1f28
franky <monster28g@gmail.com>
2016-03-25 15:35:31 +0900

    * code refactoring

--- src/main/java/com/hhi/connected/platform/controllers/GreetingController.java
+++ src/main/java/com/hhi/connected/platform/controllers/GreetingController.java
@@ -2,6 +2,7 @@ package com.hhi.connected.platform.controllers;
 
 import com.hhi.connected.platform.models.HelloMessage;
 import com.hhi.connected.platform.services.MyService;
+import com.hhi.connected.platform.services.PushService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,7 +10,6 @@ import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.scheduling.TaskScheduler;
-import org.springframework.scheduling.concurrent.ConcurrentTaskScheduler;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -23,17 +23,20 @@ public class GreetingController {
     private static final Logger LOGGER = LoggerFactory.getLogger(GreetingController.class);
 
     @Autowired
+    private
     MyService myService;
 
-////    @Autowired
-//    PushService pushService;
+    @Autowired
+    private
+    PushService pushService;
 
     private final AtomicLong counter = new AtomicLong();
 
     @Autowired
     private SimpMessagingTemplate template;
 
-    private TaskScheduler scheduler = new ConcurrentTaskScheduler();
+    @Autowired
+    private TaskScheduler scheduler;
 
     @RequestMapping(value = "/greeting", method = RequestMethod.GET )
     public Object greeting(@RequestParam(value="name", defaultValue="World") String name) {
@@ -42,20 +45,28 @@ public class GreetingController {
 
     @MessageMapping("/hello")
     @SendTo("/topic/greetings")
-    public Object hello(HelloMessage message) throws Exception {
+    public Object hello(HelloMessage message) {
         return myService.sayHello("WS, " + message.getName() + "!");
     }
 
     @PostConstruct
     private void broadcastTimePeriodically() {
-        scheduler.scheduleAtFixedRate(new Runnable() {
-            @Override public void run() {
-                tictocBroadcast();
-            }
-        }, 60000L);
+        scheduler.scheduleAtFixedRate((Runnable) this::tictoc, 60000L);
+
+        scheduler.scheduleAtFixedRate((Runnable) this::keepAlivePushService, 10000L);
+
     }
 
-    private void tictocBroadcast() {
+    private void keepAlivePushService() {
+        if(pushService.isConnected()){
+            LOGGER.debug("push service is alive");
+        }else{
+            // TODO restart pushService
+            LOGGER.debug("push service is not alive");
+        }
+    }
+
+    private void tictoc() {
         template.convertAndSend("/topic/greetings", myService.tick());
     }
 }
