db98b19039394efbe576b681bd04f05801a625d5
franky <monster28g@gmail.com>
2016-04-20 13:51:03 +0900

    * 'net::ERR_CONTENT_LENGTH_MISMATCH' 해결을 위해 RestTemplate.execute 사용하여 응답을 추출

--- src/main/java/com/hhi/connected/platform/services/RestApiServiceImpl.java
+++ src/main/java/com/hhi/connected/platform/services/RestApiServiceImpl.java
@@ -5,24 +5,48 @@ import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.HttpMessageConverterExtractor;
 import org.springframework.web.client.RestTemplate;
 
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.util.List;
 
 @Service
 public class RestApiServiceImpl implements RestApiService{
     private static final Logger LOGGER = LoggerFactory.getLogger(RestApiServiceImpl.class);
 
     @Override
-    public Object execute(String query, HttpMethod httpMethod) {
+    public Object exchange(String query, HttpMethod httpMethod) {
         try {
             LOGGER.info("query : {}", query);
             return new RestTemplate().exchange(new URI(query), httpMethod, null, Object.class);
         } catch (URISyntaxException e) {
             LOGGER.debug(e.getMessage());
             return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @Override
+    public Object execute(String query, HttpMethod httpMethod) {
+        try {
+            LOGGER.info("query : {}", query);
+            RestTemplate restTemplate = new RestTemplate();
+
+            return restTemplate.execute(new URI(query), httpMethod, null, new ApiCallResponseExtractor(Object.class, restTemplate.getMessageConverters()));
+        } catch (URISyntaxException e) {
+            LOGGER.debug(e.getMessage());
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    private class ApiCallResponseExtractor extends HttpMessageConverterExtractor<Object> {
+
+        public ApiCallResponseExtractor (Class<Object> responseType,
+                                         List<HttpMessageConverter<?>> messageConverters) {
+            super(responseType, messageConverters);
         }
     }
 }
