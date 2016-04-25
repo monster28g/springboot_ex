597be3e85386528a1e6c22097331b17185a68bc7
franky <monster28g@gmail.com>
2016-04-25 15:03:17 +0900

    * replace web socket configuration with SpringApplicationConfiguration

--- src/test/java/com/hhi/ees/platform/websocket/WebSocketTest.java
+++ src/test/java/com/hhi/ees/platform/websocket/WebSocketTest.java
@@ -1,6 +1,7 @@
 package com.hhi.ees.platform.websocket;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.hhi.ees.platform.Application;
 import com.hhi.ees.platform.models.HelloMessage;
 import com.hhi.ees.platform.models.PushServiceInfo;
 import org.apache.commons.io.FileUtils;
@@ -9,52 +10,38 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationListener;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.FilterType;
-import org.springframework.context.event.ContextRefreshedEvent;
-import org.springframework.core.env.Environment;
+import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.messaging.Message;
-import org.springframework.messaging.MessageHandler;
-import org.springframework.messaging.SubscribableChannel;
-import org.springframework.messaging.simp.annotation.support.SimpAnnotationMethodMessageHandler;
-import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.messaging.simp.stomp.StompCommand;
 import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.messaging.support.AbstractSubscribableChannel;
 import org.springframework.messaging.support.MessageBuilder;
-import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.util.JsonPathExpectationsHelper;
-import org.springframework.web.socket.config.annotation.AbstractWebSocketMessageBrokerConfigurer;
-import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
-import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 
 import java.io.File;
 import java.io.IOException;
 import java.nio.charset.Charset;
 import java.util.HashMap;
-import java.util.List;
 
 import static junit.framework.TestCase.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(classes = {
-        WebSocketTest.TestWebSocketConfig.class,
-        WebSocketTest.TestConfig.class
-})
-@TestPropertySource(
-//        locations = {"properties/gateway.properties", "properties/db.properties", "properties/push_service.properties"},
-        properties = {"push.service.duration=10"}
-)
-public class WebSocketTest {
-//    @Autowired private PushServiceInfo pushServiceInfo;
-//    @Autowired private SimpleGatewayService simpleGatewayService;
 
+// TODO replace configuration with SpringApplicationConfiguration ?
+//@ContextConfiguration(classes = {
+//        WebSocketTest.TestWebSocketConfig.class,
+//        WebSocketTest.TestConfig.class
+//})
+@SpringApplicationConfiguration(classes = Application.class)
+@TestPropertySource({
+        "/properties/gateway.properties",
+        "/properties/db.properties",
+        "/properties/push_service.properties"
+})
+public class WebSocketTest {
     @Autowired private AbstractSubscribableChannel clientInboundChannel;
 
     @Autowired private AbstractSubscribableChannel clientOutboundChannel;
@@ -112,7 +99,7 @@ public class WebSocketTest {
 
         HelloMessage helloMessage = new HelloMessage();
         helloMessage.setName(name);
-        helloMessage.setBody(getMessagesAsString("/data.json"));
+        helloMessage.setBody(getMessagesAsString("/sample_data/data.json"));
 
         byte[] payload = new ObjectMapper().writeValueAsBytes(helloMessage);
 
@@ -140,59 +127,60 @@ public class WebSocketTest {
         return FileUtils.readFileToString(new File(this.getClass().getResource(s).getFile()));
     }
 
-    @Configuration
-    @EnableScheduling
-    @ComponentScan(
-            basePackages="com.hhi.connected.platform",
-            excludeFilters = @ComponentScan.Filter(type= FilterType.ANNOTATION, value = Configuration.class)
-    )
-    @EnableWebSocketMessageBroker
-    static class TestWebSocketConfig extends AbstractWebSocketMessageBrokerConfigurer {
-
-        @Autowired
-        Environment env;
-
-        @Override
-        public void registerStompEndpoints(StompEndpointRegistry registry) {
-            registry.addEndpoint("/hivaas").withSockJS();
-        }
-
-        @Override
-        public void configureMessageBroker(MessageBrokerRegistry registry) {
-            registry.enableSimpleBroker("/queue/", "/topic/");
-//			registry.enableStompBrokerRelay("/queue/", "/topic/");
-            registry.setApplicationDestinationPrefixes("/app");
-        }
-    }
-
-    /**
-     * Configuration class that un-registers MessageHandler's it finds in the
-     * ApplicationContext from the message channels they are subscribed to...
-     * except the message handler used to invoke annotated message handling methods.
-     * The intent is to reduce additional processing and additional messages not
-     * related to the execute.
-     */
-    @Configuration
-    @SuppressWarnings("MismatchedQueryAndUpdateOfCollection")
-    static class TestConfig implements ApplicationListener<ContextRefreshedEvent> {
-
-        @Autowired
-        private List<SubscribableChannel> channels;
-
-        @Autowired
-        private List<MessageHandler> handlers;
-
-
-        @Override
-        public void onApplicationEvent(ContextRefreshedEvent event) {
-            for (MessageHandler handler : handlers) {
-                if (handler instanceof SimpAnnotationMethodMessageHandler) {
-                    continue;
-                }
-                for (SubscribableChannel channel :channels) {
-                    channel.unsubscribe(handler);
-                }
-            }
-        }
-    }
+    // TODO replace configuration with SpringApplicationConfiguration ?
+//    @Configuration
+//    @EnableScheduling
+//    @ComponentScan(
+//            basePackages="com.hhi.ees.platform",
+//            excludeFilters = @ComponentScan.Filter(type= FilterType.ANNOTATION, value = Configuration.class)
+//    )
+//    @EnableWebSocketMessageBroker
+//    static class TestWebSocketConfig extends AbstractWebSocketMessageBrokerConfigurer {
+//
+//        @Autowired
+//        Environment env;
+//
+//        @Override
+//        public void registerStompEndpoints(StompEndpointRegistry registry) {
+//            registry.addEndpoint("/hivaas").withSockJS();
+//        }
+//
+//        @Override
+//        public void configureMessageBroker(MessageBrokerRegistry registry) {
+//            registry.enableSimpleBroker("/queue/", "/topic/");
+////			registry.enableStompBrokerRelay("/queue/", "/topic/");
+//            registry.setApplicationDestinationPrefixes("/app");
+//        }
+//    }
+//
+//    /**
+//     * Configuration class that un-registers MessageHandler's it finds in the
+//     * ApplicationContext from the message channels they are subscribed to...
+//     * except the message handler used to invoke annotated message handling methods.
+//     * The intent is to reduce additional processing and additional messages not
+//     * related to the execute.
+//     */
+//    @Configuration
+//    @SuppressWarnings("MismatchedQueryAndUpdateOfCollection")
+//    static class TestConfig implements ApplicationListener<ContextRefreshedEvent> {
+//
+//        @Autowired
+//        private List<SubscribableChannel> channels;
+//
+//        @Autowired
+//        private List<MessageHandler> handlers;
+//
+//
+//        @Override
+//        public void onApplicationEvent(ContextRefreshedEvent event) {
+//            for (MessageHandler handler : handlers) {
+//                if (handler instanceof SimpAnnotationMethodMessageHandler) {
+//                    continue;
+//                }
+//                for (SubscribableChannel channel :channels) {
+//                    channel.unsubscribe(handler);
+//                }
+//            }
+//        }
+//    }
 }
